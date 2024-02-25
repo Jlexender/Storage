150fbfa0cd4c9ba142bfb78785c8c8f145090f4c
LLexender <aleksandrdmitryevich@gmail.com>
2024-02-25 21:36:59 +0300

    moved to NIO

--- src/main/java/ru/lexender/project/inbetween/ClientBridge.java
+++ src/main/java/ru/lexender/project/inbetween/ClientBridge.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import ru.lexender.project.client.Client;
 import ru.lexender.project.client.io.Output;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 /**
@@ -19,7 +20,6 @@ import java.nio.channels.SocketChannel;
 @Getter
 public class ClientBridge {
     private final Client client;
-    private final Socket socket;
     private final String hostname;
     private final int port;
     private final SocketChannel channel;
@@ -29,16 +29,16 @@ public class ClientBridge {
         this.hostname = hostname;
         this.port = port;
 
-        socket = new Socket(hostname, port);
-
         SocketAddress address = new InetSocketAddress(hostname, port);
         this.channel = SocketChannel.open(address);
     }
 
     public void send(Request request) {
         try {
-            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-            outputStream.writeObject(request);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(request);
+            channel.write(ByteBuffer.wrap(byteArrayOutputStream.toByteArray()));
         } catch (IOException exception) {
             client.getRespondent().respond(new Output("Unable to send request") {
                 @Override
@@ -51,7 +51,7 @@ public class ClientBridge {
 
     public Response get() {
         try {
-            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            ObjectInputStream inputStream = new ObjectInputStream(channel.socket().getInputStream());
             return (Response) inputStream.readObject();
         } catch (Exception exception) {
             client.getRespondent().respond(new Output("Can't get response") {
@@ -68,7 +68,8 @@ public class ClientBridge {
         System.out.printf("Connection to %s:%d has been established!\n", hostname, port);
         Response deserialized;
         do {
-            send(client.getRequest());
+            Request query = client.getRequest();
+            send(query);
             deserialized = get();
             client.getRespondent().respond(client.getTranscriber().transcribe(deserialized));
         } while (deserialized.getPrompt() != Prompt.DISCONNECTED);
--- src/main/java/ru/lexender/project/inbetween/ServerBridge.java
+++ src/main/java/ru/lexender/project/inbetween/ServerBridge.java
@@ -1,11 +1,20 @@
 package ru.lexender.project.inbetween;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.lexender.project.client.io.Input;
 import ru.lexender.project.server.Server;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
@@ -15,33 +24,75 @@ import java.nio.channels.SocketChannel;
 
 @Getter
 public class ServerBridge {
+    public static final Logger logger = LoggerFactory.getLogger(ServerBridge.class);
+
     private final Server server;
     private final int port;
-    private final ServerSocketChannel channel;
 
-    public ServerBridge(Server server, int port) throws IOException {
+    public ServerBridge(Server server, int port) {
         this.server = server;
         this.port = port;
-
-        SocketAddress address = new InetSocketAddress(port);
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.bind(address);
-
-        this.channel = serverSocketChannel;
     }
 
     public void run() {
-        try {
-            System.out.printf("Server listening on port %d\n", port);
+        try (ServerSocketChannel channel = ServerSocketChannel.open()) {
+            SocketAddress address = new InetSocketAddress(port);
+            channel.bind(address);
+            channel.configureBlocking(false);
+
+            Selector serverSelector = Selector.open();
+            channel.register(serverSelector, SelectionKey.OP_ACCEPT);
+
+
+            logger.info(String.format("Server running on PORT %d", port));
+
             for (;;) {
-                SocketChannel socketChannel = channel.accept();
-                Session session = new Session(server, socketChannel);
-                session.start();
+                SocketChannel accepted = channel.accept();
+                if (accepted != null) {
+                    logger.info("New connection");
+
+                    Response response;
+                    do {
+                        Request query = getRequest(accepted);
+                        response = server.getRequest(query);
+                        sendResponse(accepted, response);
+                    } while (response.getPrompt() != Prompt.DISCONNECTED);
+
+                    logger.info("Received request: " + getRequest(accepted));
+                }
             }
+
+
+
+        } catch (IOException exception) {
+            logger.error(exception.getMessage());
+        }
+    }
+
+
+    public Request getRequest(SocketChannel channel) {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(channel.socket().getInputStream());
+            return (Request) objectInputStream.readObject();
+        } catch (Exception exception) {
+            return new Request(new Input("") {
+                @Override
+                public String get() {
+                    return "";
+                }
+            });
+        }
+    }
+
+    public void sendResponse(SocketChannel channel, Response response) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(response);
+            channel.write(ByteBuffer.wrap(byteArrayOutputStream.toByteArray()));
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
         }
     }
-
 
 }
