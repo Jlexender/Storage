ea30a212fc36165cc0dc357d034d98cb02ab550e
LLexender <aleksandrdmitryevich@gmail.com>
2024-03-24 18:59:19 +0300

    fix

--- server/src/main/java/ru/lexender/project/server/handler/command/list/Exit.java
+++ server/src/main/java/ru/lexender/project/server/handler/command/list/Exit.java
@@ -2,12 +2,10 @@ package ru.lexender.project.server.handler.command.list;
 
 import ru.lexender.project.inbetween.Prompt;
 import ru.lexender.project.inbetween.Response;
-import ru.lexender.project.server.Server;
-import ru.lexender.project.server.exception.storage.file.transferer.StorageTransferException;
 import ru.lexender.project.server.handler.command.Command;
 import ru.lexender.project.server.handler.command.CommandStatus;
 import ru.lexender.project.server.invoker.Invoker;
-import ru.lexender.project.server.storage.transfering.ITransfer;
+
 
 /**
  * Exits the application. Saves history to file.
@@ -18,14 +16,6 @@ public class Exit extends Command {
     }
     public Response invoke(Invoker invoker) {
         setStatus(CommandStatus.IN_PROCESS);
-        try {
-            ITransfer transferer = invoker.getTransferer();
-            transferer.transferOut();
-
-            Server.logger.info("Data transfer on disconnect OK");
-        } catch (StorageTransferException exception) {
-            Server.logger.error("Data transfer on disconnect FAILED");
-        }
 
         String exitString = "Disconnected.";
 
